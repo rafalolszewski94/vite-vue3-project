e4f5e6aa9190f19dfa0e91750d29e1f51058b7ce
Rafał Olszewski <me@orafal.dev>
2022-01-27 00:53:19 +0100

    Init commit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <header
+    style="background-image: url('/mesh.png')"
+    class="relative flex h-screen items-center justify-center bg-cover bg-fixed bg-no-repeat"
+  >
+    <div class="flex flex-col items-center">
+      <h1 class="text-7xl font-black text-cyan-400">
+        <span class="text-white">Yo </span>mate!
+      </h1>
+    </div>
+  </header>
+</template>
